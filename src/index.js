6e8d8b2a8d8b674dffe3655f69dea6789aaecd0f
Charles Willian <wcharlesw15@gmail.com>
2019-12-23 20:52:48 -0300

    create file structure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <>
+      
+    </>
+  );
+}
