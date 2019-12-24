c592bc433aa8aac5fb0c7747532b92522f16dddd
Charles Willian <wcharlesw15@gmail.com>
2019-12-24 09:39:00 -0300

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,15 @@
 module.exports = {
   env: {
-    browser: true,
     es6: true,
   },
   extends: [
     'plugin:react/recommended',
     'airbnb',
-    'prettier',
-    'prettier/react'
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -23,18 +19,7 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
   ],
   rules: {
-    'prettier/prettier': 'error', // enable prettier errors
-    'react/jsx-filename-extension':[
-      'warn',
-      {
-        extensions:['.jsx', '.js']
-      }
-    ],
-    'import/prefer-default-export': 'off',
-    'no-console': ["error", { allow: ["tron"] }], // permit reactotron console.log
-    'no-param-reassign': 'off',
   },
 };
